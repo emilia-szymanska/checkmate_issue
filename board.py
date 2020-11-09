53a3458ea6fe57f2b4b2a8ff15cdbbf12aadb8ce
emilia-szymanska <emiliaszym@gmail.com>
2020-11-09 22:37:04 +0100

    Add temporary matrices

--- board.py
+++ board.py
@@ -9,6 +9,7 @@ class Board:
         self.white_figures = [] 
         self.black_figures = []
         self.matrix = np.zeros((size, size), dtype=int)
+        self.tmp = np.zeros((size, size), dtype=int)
 
 
     def add_figure(self, new_figure):
@@ -16,13 +17,54 @@ class Board:
         y = new_figure.position[1]
         if new_figure.color.name == 'white':
             self.white_figures.append(new_figure)
-            self.matrix[x][y] = Color.white.value
+#            self.matrix[x][y] = Color.white.value
         else:
             self.black_figures.append(new_figure)
-            self.matrix[x][y] = Color.black.value
+#            self.matrix[x][y] = Color.black.value
+
+
+    def apply_net(self, color):
+        if color == Color.white.name:
+            for i in self.black_figures:
+                self.tmp[i.position[0]][i.position[1]] = Color.black.value
+                self.matrix[i.position[0]][i.position[1]] = Color.black.value
+            for j in self.white_figures:
+                self.matrix[j.position[0]][j.position[1]] = Color.white.value
+                self.tmp[j.position[0]][j.position[1]] = Color.white.value
+                avail_moves, captures = j.possible_moves(self.matrix)
+                for k in avail_moves:
+                    self.tmp[k[0]][k[1]] = Color.white.value
+                for l in captures:
+                    self.tmp[l[0]][l[1]] = Color.white.value
+        else:
+            for i in self.white_figures:
+                self.tmp[i.position[0]][i.position[1]] = Color.white.value
+                self.matrix[i.position[0]][i.position[1]] = Color.white.value
+            for j in self.black_figures:
+                self.tmp[j.position[0]][j.position[1]] = Color.black.value
+                self.matrix[j.position[0]][j.position[1]] = Color.black.value
+                avail_moves, captures = j.possible_moves(self.matrix)
+                for k in avail_moves:
+                    self.tmp[k[0]][k[1]] = Color.black.value
+                for l in captures:
+                    self.tmp[l[0]][l[1]] = Color.black.value
+
+    def apply_figs(self):
+        for i in self.white_figures:
+            self.matrix[i.position[0]][i.position[1]] = Color.white.value
+        for j in self.black_figures:
+            self.matrix[j.position[0]][j.position[1]] = Color.black.value
+
+    def clear_matrix(self):
+        self.matrix = np.zeros((self.size, self.size), dtype=int)
+
+    def clear_net(self):
+        self.tmp = np.zeros((self.size, self.size), dtype=int)
+        self.matrix = np.zeros((self.size, self.size), dtype=int)
 
 
     def print_matrix(self):
         print(self.matrix)
-        #for i in range(self.size):
-        #    print(self.matrix[i])
+
+
+#    def remove_figure(self, removed_figure):
--- main.py
+++ main.py
@@ -2,27 +2,70 @@
 
 from board import *
 
-x = [
-    [0, 0, 0, 0, 0],
-    [0, 0, 0, 2, 0], 
-    [0, 0, 2, 0, 1],
-    [0, 0, 0, 1, 0],
-    [0, 0, 0, 0, 0]
-    ]
+SIZE = 8
 
+if __name__ == "__main__":
+    
+#    x = []
+#    for i in range(SIZE):
+#        for j in range(SIZE):
+#            element = input()
+#            x.append(element)
+#    print(x)
 
-p = Pawn([1, 3], 'black')
-board = Board(5)
-board.add_figure(p)
-#board.matrix[0][0] = 3
-board.print_matrix()
+    
+    board = Board(SIZE)
 
-#pos, cap = p.possible_moves(x)
-#print(pos)
-#print(cap)
+    bp = Pawn([5, 1], 'black')
+    wp = Pawn([6, 7], 'white')
+    bW = King([0, 2], 'black')
+    wW = King([7, 4], 'white')
+    wr = Rook([1, 4], 'white')
+    wq = Queen([3, 3], 'white')
+    board.add_figure(bp)
+    board.add_figure(wp)
+    board.add_figure(bW)
+    board.add_figure(wW)
+    board.add_figure(wr)
+    board.add_figure(wq)
 
-#print(x[0])
-#print(x[1])
-#print(x[2])
-#print(x[3])
-#print(x[4])
+    w_fig = board.white_figures
+
+#    board.apply_net('white')
+#    board.print_matrix()
+
+#    print(board.tmp)
+ 
+    for fig in w_fig:
+        board.apply_figs()
+        moves, captures = fig.possible_moves(board.matrix)
+        totals = moves + captures
+        previous = fig.position
+        board.clear_matrix()
+        for mov in totals:
+            fig.move(mov)
+            board.apply_net('white')
+             #   print(board.tmp)
+            if board.tmp[0][2] == Color.white.value:
+                print(fig)
+                print(fig.position)
+                print("***************")
+            board.clear_net()
+            fig.move(previous)
+
+#    def apply_figs(self):
+#    def clear_matrix(self):
+#    def clear_tmp(self):
+#    def apply_net(self, color):
+
+    #def move(self, new_pose):
+    #def remove(self):
+    #pos, cap = p.possible_moves(x)
+    #print(pos)
+    #print(cap)
+
+#        if fig == wq:
+#            print(moves)
+#            print(captures)
+#            print(totals)
+#            print(board.matrix)
